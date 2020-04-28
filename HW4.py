821b57d2428df1072997264b5f7f55ba35b93171
nliusont <62970468+nliusont@users.noreply.github.com>
2020-04-28 15:55:31 -0400

    Update HW4.py

--- HW4.py
+++ HW4.py
@@ -56,9 +56,10 @@ if __name__ == "__main__":
     rdd = sc.textFile(inputcsv)
     counts = rdd.mapPartitionsWithIndex(processTrips) \
                 .reduceByKey(lambda x,y: x+y) \
-                .sortBy(lambda x: x[1][1], ascending=False) \
+                .sortBy(lambda x: x[1], ascending=False) \
                 .map(lambda x: (x[0][0], (x[0][1], x[1]))) \
                 .reduceByKey(lambda x,y: x+y) \
+                .map(lambda x: (x[0], (x[1][0:5]))) \
                 .sortByKey() \
 
     counts.saveAsTextFile(sys.argv[2])
